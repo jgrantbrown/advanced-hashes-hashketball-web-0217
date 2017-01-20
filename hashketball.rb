ee2e46de0bf4b4cd93123241727d2dd9ccede95d
jason brown <j.grant.brown@gmail.com>
2017-01-20 19:51:13 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -7,19 +7,19 @@ def game_hash
 
   :players=>{
     "Alan Anderson"=>{
-      :number=>0,:shoe=>16, :points=>22,:rebounds=>12,:assits=>12,:steals=>3,:blocks=>1,:slam_dunks=>1},
+      :number=>0,:shoe=>16, :points=>22,:rebounds=>12,:assists=>12,:steals=>3,:blocks=>1,:slam_dunks=>1},
 
     "Reggie Evans"=>{
-      :number=>30,:shoe=>14, :points=>12,:rebounds=>12,:assits=>12,:steals=>12,:blocks=>12,:slam_dunks=>7},
+      :number=>30,:shoe=>14, :points=>12,:rebounds=>12,:assists=>12,:steals=>12,:blocks=>12,:slam_dunks=>7},
 
   "Brook Lopez"=>{
-      :number=>11,:shoe=>17, :points=>17,:rebounds=>19,:assits=>10,:steals=>3,:blocks=>1,:slam_dunks=>15},
+      :number=>11,:shoe=>17, :points=>17,:rebounds=>19,:assists=>10,:steals=>3,:blocks=>1,:slam_dunks=>15},
 
   "Mason Plumlee"=>{
-      :number=>1,:shoe=>19, :points=>26,:rebounds=>12,:assits=>6,:steals=>13,:blocks=>8,:slam_dunks=>5},
+      :number=>1,:shoe=>19, :points=>26,:rebounds=>12,:assists=>6,:steals=>3,:blocks=>8,:slam_dunks=>5},
 
     "Jason Terry"=>{
-      :number=>31,:shoe=>15, :points=>19,:rebounds=>2,:assits=>2,:steals=>4,:blocks=>11,:slam_dunks=>1}
+      :number=>31,:shoe=>15, :points=>19,:rebounds=>2,:assists=>2,:steals=>4,:blocks=>11,:slam_dunks=>1}
   }
 
 
@@ -27,20 +27,21 @@ def game_hash
 
   :players=>{
     "Jeff Adrien"=>{
-      :number=>4,:shoe=>18, :points=>10,:rebounds=>1,:assits=>1,:steals=>2,:blocks=>7,:slam_dunks=>2},
+      :number=>4,:shoe=>18, :points=>10,:rebounds=>1,:assists=>1,:steals=>2,:blocks=>7,:slam_dunks=>2},
 
     "Bismak Biyombo"=>{
-      :number=>0,:shoe=>16, :points=>12,:rebounds=>4,:assits=>7,:steals=>7,:blocks=>15,:slam_dunks=>10},
+      :number=>0,:shoe=>16, :points=>12,:rebounds=>4,:assists=>7,:steals=>7,:blocks=>15,:slam_dunks=>10},
 
   "DeSagna Diop"=>{
-      :number=>2,:shoe=>14, :points=>24,:rebounds=>12,:assits=>12,:steals=>4,:blocks=>5,:slam_dunks=>5},
+      :number=>2,:shoe=>14, :points=>24,:rebounds=>12,:assists=>12,:steals=>4,:blocks=>5,:slam_dunks=>5},
 
   "Ben Gordon"=>{
-      :number=>8,:shoe=>15, :points=>33,:rebounds=>3,:assits=>2,:steals=>1,:blocks=>1,:slam_dunks=>0},
+      :number=>8,:shoe=>15, :points=>33,:rebounds=>3,:assists=>2,:steals=>1,:blocks=>1,:slam_dunks=>0},
 
     "Brendan Haywood"=>{
-      :number=>33,:shoe=>15, :points=>6,:rebounds=>12,:assits=>12,:steals=>22,:blocks=>5,:slam_dunks=>12}
-  }}
+      :number=>33,:shoe=>15, :points=>6,:rebounds=>12,:assists=>12,:steals=>22,:blocks=>5,:slam_dunks=>12}
+  }
+}
   }
 end
 
@@ -72,14 +73,16 @@ def team_names
   new_array.flatten
 end
 
-=begin
-def team_colors(team_name)
-  game_hash.map{|location,team_data|
-    team_data.map{|attribute,data| return team_data[:colors] if team_name=team_data[:team_name]
-    }
-  }
+def team_colors(teamname)
+    game_hash.map{|location,team_data|
+        team_data.map{|attribute,data|  if attribute==:team_name #look in attribute for :team_name
+           data.map {|team,stats|  if team==teamname #string matches player key
+           return team_data[:colors]
+      end }
+      end }
+          }
 end
-=end
+
 def player_numbers(team)
   if team=="Charlotte Hornets"
   game_hash[:away][:players].map{|k,v|  v[:number]}
@@ -97,3 +100,27 @@ end
         end }
           }
   end
+
+
+
+=begin
+  def big_shoe_rebounds
+
+      new_array=[ ]
+      game_hash.collect{|location,team_data|
+          team_data.collect{|attribute,data|  if attribute==:players
+             data.collect{|player,stats|
+             new_array<<stats[:shoe]}
+      end}
+         }
+       biggestshoe=new_array.sort.last
+
+      game_hash.collect{|location,team_data|
+            team_data.collect{|attribute,data| if  attribute==:players
+            data.collect{|player,stats| puts stats[:rebounds] if biggestshoe==stats[:shoe]
+            }
+          end
+          }
+      }
+      end
+    =end
